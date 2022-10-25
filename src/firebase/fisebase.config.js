edb05fd085f030ba350431d0e199e96a77bac79e
NayeemHossenn <nayeemhossen245@gmail.com.com>
2022-10-25 23:13:51 +0600

    folder structure added

---
+++ src/firebase/fisebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAkCwZwmRGVy-KEcBPWGJXlvcIq79aeKKk",
+  authDomain: "tutorial-points-7c952.firebaseapp.com",
+  projectId: "tutorial-points-7c952",
+  storageBucket: "tutorial-points-7c952.appspot.com",
+  messagingSenderId: "253392158330",
+  appId: "1:253392158330:web:c13dbfd5afd431e76d45c3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/layout/Main.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
+import Header from "../Shared/Header/Header";
+
+const Main = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Container>
+        <Row>
+          <Col lg="4">
+            <LeftSideNav></LeftSideNav>
+          </Col>
+          <Col lg="8">
+            <Outlet></Outlet>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Main;
